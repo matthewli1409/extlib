0d242f0c9392be3f77beeb6d226282d611375bfc
matthewli1409 <matthewli1409@gmail.com>
2020-10-06 15:16:53 +0700

    tz fix

--- extlib/utils.py
+++ extlib/utils.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone
 
 
 def convert_dict_keys_from_ms_to_dt(dict):
@@ -12,7 +12,7 @@ def convert_dict_keys_from_ms_to_dt(dict):
     """
     new_dict = {}
     for key in dict:
-        dt = datetime.datetime.fromtimestamp(float(key)/1000.0)
+        dt = datetime.fromtimestamp(float(key)/1000.0, tz=timezone.utc)
         new_key = f"{dt.year}, {dt.month}, {dt.day}"
         new_dict[new_key] = dict[key]
     return new_dict
