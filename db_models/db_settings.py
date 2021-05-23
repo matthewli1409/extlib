1617301494322c15151e9bbab1c334f1e6dd1dbc
matthewli1409 <matthewli1409@gmail.com>
2021-05-23 15:10:27 +0700

    fix

---
+++ db_models/db_settings.py
@@ -0,0 +1,41 @@
+from main import ConnectMongoDB
+
+
+class DBSettings(ConnectMongoDB):
+    def __init__(self, db, client, strat):
+        super().__init__(db, client)
+        self.mongo_client = super().get_mongo_client()
+        self.strat = strat
+
+    def get_strat_info_db(self, strat):
+        """Get strat information
+
+        Arguments:
+            strat {str} -- strategy name "MA_BO_I"
+
+        Keyword Arguments:
+            mongo_client {client} -- use user provided mongo client else function will call itself (default: {None})
+
+        Returns:
+            list -- list of strat information
+        """
+        return list(self.mongo_client['settings'].find({'strategy': strat}))[0]
+
+    def get_last_run(self):
+        """Get last time the strategy was ran
+
+        Returns:
+            datetime.datetime -- datetime of the last time the strat was ran
+        """
+        strat_info = self.get_strat_info_db(self.strat)
+        return strat_info.get('lastRun')
+
+    def set_last_run(self, dt):
+        """Get last time the strategy was ran
+
+        Arguments:
+            dt {datetime.datetime} -- datetime to set
+        """
+        query = {'strategy': self.strat}
+        newvalues = {'$set': {'lastRun': dt}}
+        self.mongo_client['settings'].update_one(query, newvalues)
--- db_models/main.py
+++ db_models/main.py
@@ -1,13 +1,16 @@
-import os
-
 import pymongo
 
 
-def get_mongo_client():
-    """Get correct mongo client
+class ConnectMongoDB:
+    def __init__(self, db, client):
+        self.client = client
+        self.db = db
 
-    Returns:
-        pymongo.database.Database -- client that points to db
-    """
-    mongo_client = pymongo.MongoClient(os.environ['MONGODB_CLIENT'])
-    return mongo_client[MONGODB_DB]
+    def get_mongo_client(self):
+        """Get correct mongo client
+
+        Returns:
+            pymongo.database.Database -- client that points to db
+        """
+        mongo_client = pymongo.MongoClient(self.client)
+        return mongo_client[self.db]
