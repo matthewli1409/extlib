0ed97f7849a79e5c6fb0083a9eeeb7b3b8ca83b4
matthewli1409 <matthewli1409@gmail.com>
2021-05-25 08:15:51 +0700

    tr

--- logger/logger.py
+++ logger/logger.py
@@ -8,12 +8,12 @@ from settings import REDIS_HOST, REDIS_PORT, REDIS_LOG_LIST
 def log_generic_msg(data, log_lvl='INFO'):
     """log helper for generic message"""
     r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
-    data_to_push = json.dumps({'logType': 'GENERIC', 'logLvl': log_lvl, 'data': data})
+    data_to_push = json.dumps({'logType': 'GENERIC', 'logLvl': log_lvl, 'data': str(data)})
     r.lpush(REDIS_LOG_LIST, data_to_push)
 
 
 def log_error_msg(data, log_lvl='ERROR'):
     """log helper for error message"""
     r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
-    data_to_push = json.dumps({'logType': 'ERROR', 'logLvl': log_lvl, 'data': data})
+    data_to_push = json.dumps({'logType': 'ERROR', 'logLvl': log_lvl, 'data': str(data)})
     r.lpush(REDIS_LOG_LIST, data_to_push)
