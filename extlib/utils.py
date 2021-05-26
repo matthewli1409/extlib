f292497416ad6513bd5ee62bc115df9ad2d1c4b9
matthewli1409 <matthewli1409@gmail.com>
2021-05-26 11:39:09 +0700

    snake

--- extlib/utils.py
+++ extlib/utils.py
@@ -16,3 +16,16 @@ def convert_dict_keys_from_ms_to_dt(dict_data):
         new_key = f"{dt.year}, {dt.month}, {dt.day}"
         new_dict[new_key] = dict_data[key]
     return new_dict
+
+
+def snake_to_canel(snake_str):
+    """Transform snake case to camel case used in mongo
+
+    Args:
+        snake_str {str} -- string to transform
+    """
+    snake_str = snake_str.lower()
+    components = snake_str.split('_')
+    # We capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return components[0] + ''.join(x.title() for x in components[1:])
