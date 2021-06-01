1b47d7f42099aecf9ddbb096aadf3e7b96bec084
matthewli1409 <matthewli1409@gmail.com>
2021-06-01 08:36:19 +0700

    log fix

--- logger/logger.py
+++ logger/logger.py
@@ -24,3 +24,10 @@ def log_execution_msg(data, log_lvl='INFO'):
     r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
     data_to_push = json.dumps({'logType': 'EXECUTION', 'logLvl': log_lvl, 'data': str(data)})
     r.lpush(REDIS_LOG_LIST, data_to_push)
+
+
+def log_funding_msg(data, log_lvl='INFO'):
+    """log helper for funding message"""
+    r = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+    data_to_push = json.dumps({'logType': 'FUNDING', 'logLvl': log_lvl, 'data': str(data)})
+    r.lpush(REDIS_LOG_LIST, data_to_push)
